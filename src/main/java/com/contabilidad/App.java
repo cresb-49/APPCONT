48eb1a3c09e6957670da20117ceeb9c8baa99734
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-08-25 01:34:25 -0600

    First commit

---
+++ src/main/java/com/contabilidad/App.java
@@ -0,0 +1,17 @@
+package com.contabilidad;
+
+/**
+ * PROGRAMA DE CONTABILIDAD VERSION ALFA 0.0.1
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     *  Clase Principal del programa de cotabilidad
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos");
+    }
+}
